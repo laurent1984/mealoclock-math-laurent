4f98f196e89bd297218fa36ffffd468b6e1b1120
laurent laporte <laportelaurentoc@gmail.com>
2018-05-04 18:01:59 +0200

    maj3

--- app/Views/event/list.php
+++ app/Views/event/list.php
@@ -12,8 +12,8 @@
             <img src="<?= $basePath ?>assets/images/communities/vegan.jpg" alt="" class="img-fluid">
         </div>
         <div class="col-12 col-md-6 box-content">
-            <h2><?= $currentEvent->getTitle() ?></h2>
-            <p><?= $currentEvent->getDescription() ?></p>
+            <h2></h2>
+            <p></p>
         </div>
         <div class="col-12 col-md-3 area">
             <h3>2018-06-10</h3>
